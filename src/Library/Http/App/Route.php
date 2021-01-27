0507d5b1253a82ee16ca55de4c24f37e2ca372a6
wolfans <wolfans@wolfansdeMacBook-Pro.local>
2021-01-27 18:52:59 +0800

    增加配置

--- src/Library/Http/App/Route.php
+++ src/Library/Http/App/Route.php
@@ -37,7 +37,7 @@ class Route {
             foreach ($this->allHttpPosts as $port) {
                 $json = file_get_contents('http://' . $ip . ':' . $port . '/json');
                 $arr  = @json_decode($json, true);
-                is_array($arr) && $task[$ip] = $arr;
+                is_array($arr) && $task[$ip . ':' . $port] = $arr;
             }
         }
 
@@ -67,11 +67,12 @@ class Route {
             }
             </style>';
         $html .= '<table class="gridtable">';
-        $html .= '<tr> <th>任务</th> <th>最大</th><th>最小</th><th>loop</th><th>sleep</th> <th>历史启动</th><th>总/平时时间</th><th>运行量</th> </tr>';
+        $html .= '<tr> <th>节点</th><th>任务</th> <th>最大</th><th>最小</th><th>loop</th><th>sleep</th> <th>历史启动</th><th>总/平时时间</th><th>运行量</th> </tr>';
         foreach ($task as $ip => $schedule) {
             foreach (Table::getShareSchedule() as $options) {
                 $aa   = $options['history_exec_num'] ? $options['history_exec_num'] : 1;
                 $html .= '<tr> <td>' .
+                    $ip . '</td> <td>' .
                     $options['route_id'] . '</td> <td>' .
                     $options['max_pnum'] . '</td><td>' .
                     $options['min_pnum'] . '</td><td>' .
